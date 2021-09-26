fcc913f661a04875e72bfc4e36bfda60d5ff2708
architjain123 <46052746+architjain123@users.noreply.github.com>
2021-09-26 13:23:22 -0700

    added more functionality

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
 from configparser import ConfigParser
+
 config = ConfigParser()
 config.read("config.txt")
 address = config.get("DB", "address")
@@ -9,20 +11,59 @@ username = config.get("DB", "username")
 password = config.get("DB", "password")
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient('mongodb://{}:{}@{}'.format(username, password, address), 27017)
 db = client["main"]
 patterns = db['patterns']
+cache = db['cache']
 
-@app.route('/get/graphdata/allfoot', methods=['GET'])
-def get_all_foottraffic():
 
-    cursor = patterns.aggregate([{"$group": {"_id": "$date", "total": { "$sum": "$visits" }}},\
+@app.route('/get/overall', methods=['GET'])
+def overall():
+    # cursor = patterns.aggregate([{"$group": {"_id": "$date", "total": {"$sum": "$visits"}}},\
+    #                              {"$sort": {"_id": 1}}])
+    # result = []
+    # for obj in cursor:
+    #     result.append([obj["_id"]*1000, obj["total"]])
+    # cache.insert_one({"type": "overall", "data": result})
+
+    obj = cache.find_one({"type": "overall"})
+    return jsonify({"data": obj["data"]})
+
+
+@app.route('/get/naics/<naics_code>', methods=['GET'])
+def naics(naics_code):
+    cursor = patterns.aggregate([{"$match": {"naics_code": {"$regex": '^' + naics_code}}},
+                                 {"$group": {"_id": "$date", "total": {"$sum": "$visits"}}},
                                  {"$sort": {"_id": 1}}])
     result = []
     for obj in cursor:
-        result.append({"date": str(obj["_id"].isoformat()), "total": obj["total"]})
-    return jsonify(result)
+        result.append([obj["_id"] * 1000, obj["total"]])
+    return jsonify({"data": result})
+
+
+@app.route('/get/postal/<postal_code>', methods=['GET'])
+def postal(postal_code):
+    cursor = patterns.aggregate([{"$match": {"postal_code": postal_code}},
+                                 {"$group": {"_id": "$date", "total": {"$sum": "$visits"}}},
+                                 {"$sort": {"_id": 1}}])
+    result = []
+    for obj in cursor:
+        result.append([obj["_id"] * 1000, obj["total"]])
+    return jsonify({"data": result})
+
+
+@app.route('/get/name/<business_name>', methods=['GET'])
+def name(business_name):
+    cursor = patterns.aggregate([{"$match": {"location_name": business_name}},
+                                 {"$group": {"_id": "$date", "total": {"$sum": "$visits"}}},
+                                 {"$sort": {"_id": 1}}])
+    result = []
+    for obj in cursor:
+        result.append([obj["_id"] * 1000, obj["total"]])
+    return jsonify({"data": result})
+
 
 
 if __name__ == '__main__':
--- upload_data.py
+++ upload_data.py
@@ -1,49 +1,113 @@
-import csv
-import json
+import csv, json, re, zipfile, shutil, gzip
 from configparser import ConfigParser
-from datetime import datetime
+from datetime import datetime, timezone
 from pymongo import MongoClient
-import zipfile
+from os import walk
+
+
+def get_files_to_parse():
+    files_to_parse = []
+    with open('parsed_files.json', 'r') as f:
+        json_file = json.loads(f.read())
+        parsed_files = set(json_file["files"])
+        files_in_dir = next(walk("files"), (None, None, []))[2]
+
+        for filename in files_in_dir:
+            if filename not in parsed_files and filename.startswith("CA-CORE_POI-PATTERNS"):
+                files_to_parse.append(filename)
+
+    return files_to_parse
+
+
+def parse_year_month(filename):
+    filesplit = filename.split("-")[3]
+    year = filesplit[:4]
+    month = filesplit[-2:]
+    return int(year), int(month)
+
+
+def unzipGzFile(gz_file):
+    try:
+        with gzip.open(gz_file, 'rb') as f_in:
+            with open(gz_file.replace('.gz', ""), 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    except:
+        return
+
+
+def parse_upload_file(filename, patterns):
+
+    shutil.rmtree("temp")
+    with zipfile.ZipFile("files/" + filename, 'r') as zip_ref:
+        zip_ref.extractall("temp")
+
+    unzipGzFile("temp/core_poi-patterns.csv.gz")
+    csv_file = "temp/core_poi-patterns.csv"
+    norm_file = "temp/normalization_stats.csv"
+
+    normalization = {}
+    with open(norm_file, encoding="utf-8") as patterns_file:
+        norm_reader = csv.DictReader(patterns_file, delimiter=',')
+        line_count = 0
+        for row in norm_reader:
+            if line_count != 0 and row["region"] == "ca":
+                normalization[int(row["day"])] = int(row["total_visits"])
+            line_count += 1
+
+    with open(csv_file, encoding="utf-8") as patterns_file:
+        csv_reader = csv.DictReader(patterns_file, delimiter=',')
+        line_count = 0
+        documents = []
+        for row in csv_reader:
+            if line_count != 0 and row["visits_by_day"]:
+                for idx, val in enumerate(json.loads(row["visits_by_day"])):
+                    date = int(datetime(year, month, idx + 1).replace(tzinfo=timezone.utc).timestamp())
+                    doc = {"placekey": row["placekey"], "location_name": row["location_name"],
+                           "naics_code": row["naics_code"], "postal_code": row["postal_code"], "date": date,
+                           "visits": val, "normalization": normalization[idx + 1]}
+                    documents.append(doc)
+
+            line_count += 1
+            if line_count % 1000 == 0:
+                print(line_count)
+
+            if len(documents) > 99000:
+                patterns.insert_many(documents)
+                documents = []
+
+        if len(documents) > 0:
+            patterns.insert_many(documents)
+
+        return True
+
+
+def add_to_parsed_files(filename):
+    with open('parsed_files.json', 'r+') as f:
+        data = json.load(f)
+        data['files'].append(filename)
+        f.seek(0)  # reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()  # remove remaining part
+
 
 config = ConfigParser()
 config.read("config.txt")
-address = config.get("DB", "address")
-username = config.get("DB", "username")
-password = config.get("DB", "password")
+address, username, password = config.get("DB", "address"), config.get("DB", "username"), config.get("DB", "password")
 
 client = MongoClient('mongodb://{}:{}@{}'.format(username, password, address), 27017)
-db = client["main"]
-patterns = db['patterns']
+patterns = client["main"]['patterns']
 
-filename = "CA-CORE_POI-PATTERNS-2021_06-2021-07-29.zip"
-year = 2021
-month = 6
+files_to_parse = get_files_to_parse()
+if not files_to_parse:
+    print("No files to parse")
+    quit()
+else:
+    print("Parsing files : ", files_to_parse)
 
-with zipfile.ZipFile("files/" + filename, 'r') as zip_ref:
-    zip_ref.extractall("temp")
-
-csvfile = "temp/core_poi-patterns.csv"
-normfile = "temp/normalization_stats.csv"
-
-normalization = {}
-with open(normfile, encoding="utf-8") as patterns_file:
-    norm_reader = csv.DictReader(patterns_file, delimiter=',')
-    line_count = 0
-    for row in norm_reader:
-        if line_count != 0 and row["region"] == "ca":
-            normalization[int(row["day"])] = int(row["total_visits"])
-        line_count += 1
-
-with open(csvfile, encoding="utf-8") as patterns_file:
-    csv_reader = csv.DictReader(patterns_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0 and row["visits_by_day"]:
-            docs = []
-            for idx, val in enumerate(json.loads(row["visits_by_day"])):
-                doc = {"placekey": row["placekey"], "location_name": row["location_name"], "naics_code": row["naics_code"], "postal_code": row["postal_code"], "date": datetime(year, month, idx+1), "visits": val, "normalization": normalization[idx+1]}
-                docs.append(doc)
-            patterns.insert_many(docs)
-        line_count += 1
-        if line_count % 100 == 0:
-            print(line_count)
+for filename in files_to_parse:
+    year, month = parse_year_month(filename)
+    print("Parsing", filename)
+    if parse_upload_file(filename, patterns):
+        add_to_parsed_files(filename)
+    else:
+        print("Unable to upload file: " + filename)
